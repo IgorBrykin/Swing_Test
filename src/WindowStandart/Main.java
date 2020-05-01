900d4c08209410487eac03276447085f71ad7c5b
IgorBrykin <igorbrikin@gmail.com>
2020-05-01 14:36:50 +0300

    Initial commit

---
+++ src/WindowStandart/Main.java
@@ -0,0 +1,9 @@
+package WindowStandart;
+
+public class Main {
+
+    public static void main(String[] args) {
+	MyWindow myWindow = new MyWindow();
+	MyWindowTwo myWindowTwo = new MyWindowTwo();
+    }
+}
